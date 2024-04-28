12f462bac0e6cb8a48dcfc7ad1e887dfd3454ddc
Vincent Hoang <vhoang8@uci.edu>
2024-04-27 22:20:39 -0700

    Added compressed image function

--- vertex_ai.py
+++ vertex_ai.py
@@ -1,5 +1,8 @@
 from google.cloud import aiplatform
 import os
+import io
+from PIL import Image
+from pathlib import Path
 from dotenv import load_dotenv
 import base64
 load_dotenv()
@@ -12,6 +15,37 @@ image_path = ""  # CHANGE TO USER PHOTO INPUT
 from google.cloud import aiplatform
 from google.cloud.aiplatform.gapic.schema import predict
 
+def compress_image(input_file_path, max_size=1500000, quality_step=5):
+    """
+    Compress an image and save it such that its size does not exceed max_size.
+    
+    Args:
+    input_file_path (str): The path to the original image file.
+    max_size (int, optional): Maximum file size in bytes. Defaults to 1.5 MB.
+    quality_step (int, optional): The decremental step in quality during compression iterations. Defaults to 5.
+    """
+    # Open the image
+    img = Image.open(input_file_path)
+    
+    # In-memory file-like object
+    img_bytes = io.BytesIO()
+    
+    # Initial compression attempt
+    quality = 95
+    while True:
+        img_bytes.seek(0)  # Rewind to the beginning of the file-like object
+        img.save(img_bytes, format='JPEG', quality=quality)
+        size = img_bytes.tell()  # Get the size of the in-memory file
+        if size <= max_size or quality <= 10:
+            break
+        quality -= quality_step  # Decrease quality to further reduce file size
+    
+    # Save the final compressed image
+    output_file_path = str(Path.cwd()) + "/bearhack24/compressed_pics/file.jpg"
+    img_bytes.seek(0)  # Rewind to the beginning of the file-like object
+    img = Image.open(img_bytes)
+    img.save(output_file_path, 'JPEG')
+    return output_file_path
 
 def predict_image_classification_sample(
     project: str,
@@ -50,15 +84,15 @@ def predict_image_classification_sample(
     # See gs://google-cloud-aiplatform/schema/predict/prediction/image_classification_1.0.0.yaml for the format of the predictions.
     predictions = response.predictions
     for prediction in predictions:
-        #print(" prediction:", dict(prediction))
-        result = dict(prediction)
-        diagnosis = result["displayNames"]
-        print(diagnosis)
+        print(" prediction:", dict(prediction))
+        # result = dict(prediction)
+        # diagnosis = result["displayNames"]
+        # print(diagnosis)
 
 
 predict_image_classification_sample(
     project="1033084311617",
     endpoint_id="2243186239593250816",
     location="us-central1",
-    filename=image_path
+    filename=compress_image(image_path)
 )
